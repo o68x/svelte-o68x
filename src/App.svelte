fb04756f3b95ca411ce5db8fc2128635203599b0
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-11-12 10:25:58 +0100

    add "hero" with grid

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,38 @@
 <script>
   import Navbar from './components/navbar.svelte';
+  import Hero from './components/hero.svelte';
   export let name;
 </script>
 
 <style lang="scss" global>
-  @import  "./style/global.scss";
+  @import "./style/global.scss";
 </style>
 
-<Navbar {name}/>
+<section class="about">
+  <Navbar {name} />
 
-<h1>This is the {name.toUpperCase()} (aka Olivier Cardinaux) interweb!</h1>
+  <div>
+    <h1>This is the {name.toUpperCase()} (aka Olivier Cardinaux) interweb!</h1>
 
-<p>
-  Here we will have a homepage, a about (resume) and a projects page.
-</p>
+    <p>
+      Here we will have a homepage, a about (resume) and a projects page.
+    </p>
 
-<p>Then also some photo gallery, and a blog</p>
+    <p>Then also some photo gallery (lampadaires), and a blog</p>
 
-<p>
-  A lot of links to my writings.
-</p>
+    <p>
+      A lot of links to my writings.
+    </p>
 
-<p>
-  Some client work, github, etc.
-</p>
+    <p>
+      Some client work, github, etc.
+    </p>
+  </div>
 
+  <Hero />
+  </section>
+  <section>
+    <p>
+      More stuff…
+    </p>
+  </section>
