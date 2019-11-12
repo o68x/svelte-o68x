ed8eea9285568107b709b02412699917ba757320
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-11-12 11:28:40 +0100

    small changes to navbar

--- src/components/hero.svelte
+++ src/components/hero.svelte
@@ -12,14 +12,16 @@
       text-align: center;
       &.letter {
         color: $primary-color;
-        opacity: 0.6;
+        opacity: 0.5;
+        // TODO: add calc() to get more sensible sizes
+        // https://css-tricks.com/fun-viewport-units/#article-header-id-1
         font-size: 3.1875vw;
       }
       &.number {
         font-size: 1.59375vw;
         color: $primary-color;
-        opacity: 0.4;
-        margin-top: -1em;
+        opacity: 0.3;
+        margin-top: -0.8em;
       }
       &.highlight {
         color: $primary-color;
@@ -31,6 +33,7 @@
 </style>
 
 <div class="grid-68">
+   <!-- TODO: Drop "O" and "x" ? -->
   <span class="letter highlight">O</span>
   <span class="letter">l</span>
   <span class="letter">i</span>
--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -5,13 +5,13 @@
 <header class="navbar">
   <section class="navbar-section">
     <a href="#" class="navbar-brand mr-2">{name}</a>
-    <a class="btn btn-link" href="#">News</a>
-    <a class="btn btn-link" href="#">Sports</a>
-    <a class="btn btn-link disabled" href="#">Finance</a>
+    <a class="btn btn-link" href="#">projets</a>
+    <a class="btn btn-link" href="#">cv</a>
+    <a class="btn btn-link disabled" href="#">lampadaires</a>
   </section>
   <section class="navbar-section">
     <span class="mr-2">This is what?</span>
-    <span>No icons here! <i class="icon icon-message"></i></span>
+    <span>No fucking icons here! <i class="icon icon-message"></i></span>
   </section>
 </header>
 
